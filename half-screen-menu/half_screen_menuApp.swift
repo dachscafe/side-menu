e2274794021ec6c7ff086a06f04222e58f406bff
Cafe <dachscafe@yahoo.co.jp>
2023-04-24 20:10:45 +0900

    Initial Commit

---
+++ half-screen-menu/half_screen_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  half_screen_menuApp.swift
+//  half-screen-menu
+//
+//  Created by Cafe on 2023/04/24.
+//
+
+import SwiftUI
+
+@main
+struct half_screen_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
